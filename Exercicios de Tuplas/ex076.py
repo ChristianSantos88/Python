a261fb74ad7ae5a31f2acf075b682ddeb758ea2b
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-01-18 17:34:41 -0300

    ex076 feito

--- Exercicios de Tuplas/ex076.py
+++ Exercicios de Tuplas/ex076.py
@@ -2,3 +2,10 @@
 Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 '''
+
+names_and_prices = ('Caneta', 1.50, 'Borracha', 0.75, 'Lapiseira', 1.20, 'Mochila', 89.90)
+produto = 0
+
+for p in range(1, len(names_and_prices), 2):
+    print(f'{names_and_prices[produto]:<10}.......... R${names_and_prices[p]}')
+    produto += 2
