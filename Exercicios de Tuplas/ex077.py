ae7c3cf9bb7739d220c39f325d53718e15877a25
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-01-19 18:30:35 -0300

    Update ex077.py

--- Exercicios de Tuplas/ex077.py
+++ Exercicios de Tuplas/ex077.py
@@ -6,8 +6,9 @@ Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 palavras = ('caneta', 'lapiseira', 'forceps', 'cabelo', 'baqueta', 'apagador', 'remedio', 'vislumbre', 'datilografia')
 
 for p in palavras:
-    print(f'Na palavra {p.upper()} temos: ', end=' ')
-    for letra in p:
-        if letra in 'aeiou':
-            print(letra, end=' ')
+    print(f'Na palavra {p.upper()}, temos: ', end=' ')
+    for letras in p:
+        if letras in 'aeiou':
+            print(letras, end=' ')
     print()
+
