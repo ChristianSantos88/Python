01422acb9f72145c96d74879655e8d8a658005b7
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-01-21 22:27:42 -0300

    more stuff

--- Exercícios de Lista (parte 01)/ex080.py
+++ Exercícios de Lista (parte 01)/ex080.py
@@ -1,2 +1,14 @@
 # Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição corre-
 # ta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
+
+lista = []
+maior_num = 0
+
+for v in range(0, 5):
+    num = int(input('Digite um valor: '))
+    if v == 0:
+        maior_num = num
+        lista.append(num)
+
+
+
--- Exercícios de Lista (parte 01)/ex081.py
+++ Exercícios de Lista (parte 01)/ex081.py
@@ -2,3 +2,21 @@
 # a) Quantos números foram digitados;
 # b) A lista de valores, ordenada de forma DECRESCENTE;
 # c) Se o valor 5 foi digitado e está ou não na lista.
+
+lista = []
+
+while True:
+    n = int(input('Digite um valor: '))
+    lista.append(n)
+
+    perg = input('Gostaria de continuar? ')
+    if perg == 'n':
+        break
+
+lista.sort(reverse=True)
+print(f'Você digitou {len(lista)} elementos.')
+print(f'Valores em ordem decrescente: {lista}')
+if 5 in lista:
+    print('O valor 05 foi digitado.')
+else:
+    print('O valor 05 não foi digitado.')
