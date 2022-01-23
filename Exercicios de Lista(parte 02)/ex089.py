5e15eef21632ba206fe7b3b7c8a32d43df9c03ad
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-01-23 15:16:17 -0300

    aula17
    Aula 17 do curso de Python, Mundo 03, do canal Curso em Vídeo.

--- Exercicios de Lista(parte 02)/ex089.py
+++ Exercicios de Lista(parte 02)/ex089.py
@@ -1,4 +1,4 @@
 # Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um
 # boletim contendo a média de cada um e ue permita qo usuário possa mostrar as notas de cada individualmente. (33'00)
 # Dica: Fazer uma lista grande em que o NOME será o 1º elemento e as duas notas serão o 2º elemento.
-# [[[nota1, nota2]]]
+# [[nomeA[nota1, nota2]], [nomeB[nota1, nota2]], [nomeC[nota1, nota2]]]
