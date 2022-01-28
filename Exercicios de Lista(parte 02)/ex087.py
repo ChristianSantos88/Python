d3d0527af07bc389f1ffe900eb05802662b56b63
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-01-28 00:09:08 -0300

    ex087 pronto

--- Exercicios de Lista(parte 02)/ex087.py
+++ Exercicios de Lista(parte 02)/ex087.py
@@ -3,56 +3,85 @@
 # B) A soma dos valores da terceira coluna;
 # C) O maior valor da segunda linha.
 
+
 matriz = []
 temp = []
-cont = 1
-pos = 0
+cont = line = 1
+pos = soma = soma_col3 = maior_valor = 0
 
+#  Inserindo os elementos na matriz
 while cont <= 3:
-    for m in range(0, 3):
-        temp.append(int(input('Digite um valor: ')))
+    for col in range(0, 3):
+        temp.append(int(input(f'Digite um valor para [{line},{col + 1}]: ')))
     matriz.append(temp.copy())
     cont += 1
+    line += 1
     temp.clear()
 
+#  Lendo a matriz
+for line in range(0, 3):
+    soma_col3 += matriz[line][2]
+    print()
+    for col in range(0, 3):
+        print(f'[{matriz[line][col]:^5}]', end=' ')
+        if matriz[line][col] % 2 == 0:
+            soma += matriz[line][col]
+
+print()
+
+# soma de todos os valores pares digitados
+print(f'\nSoma dos elementos pares: {soma}')
+
+# soma dos valores da terceira coluna
+print(f'\nSoma dos elementos da terceira coluna: {soma_col3}')
+
+# maior valor da segunda linha
+for m in matriz[1]:
+    if m > maior_valor:
+        maior_valor = m
+print(f'\nMaior valor da segunda linha: {maior_valor}')
+
+'''
 for m in range(0, len(matriz)):
     print(f'[{matriz[m][pos]}]', end=' ')
     pos += 1
-
-
-
-
-
-
-
-
-
-
+'''
 
 '''
 matriz = list()
+temp = list()
 coluna3 = []
 cont = 0
 soma = soma_col3 = 0
 
+#  Inserindo os valores na matriz
 for e in range(0, 3):
     for m in range(0, 3):
-        matriz.append(int(input(f'Digite um valor para [{e+1},{m+1}]: ')))
+        temp.append(int(input(f'Digite um valor para [{e+1},{m+1}]: ')))
+
+#  Lendo a matriz
+for line in range(0, 3):
+    print()
+    for col in range(0, 3):
+        print(f'[{matriz[line]}]', end=' ')
+
+print()
+print(matriz)
 
 for m in range(2, len(matriz), 3):
     soma_col3 += matriz[m]
 
 print()
 
-for m in range(0, len(matriz)):
-    print(f'[{matriz[m]:^5}]', end=' ')
-    cont += 1
-    if cont == 3:
-        print()
-        cont = 0
+#  for m in range(0, len(matriz)):
+#   print(f'[{matriz[m]:^5}]', end=' ')
+#   cont += 1
+#   if cont == 3:
+#       print()
+#       cont = 0
 
-    if matriz[m] % 2 == 0:
-        soma += matriz[m]
+#   if matriz[m] % 2 == 0:
+#       soma += matriz[m]
 
 print(f'A soma dos valores pares digitados é igual a: {soma}')
 print(f'A soma dos valores da terceira coluna é igual a: {soma_col3}')
