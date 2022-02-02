7e31147c00dae4f5e5a51a21620a1d8b406a4759
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-02-02 16:58:46 -0300

    ex090 done (again)

--- Exercícios de Dicionários/ex090.py
+++ Exercícios de Dicionários/ex090.py
@@ -1,10 +1,9 @@
 # Faça um exercício que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o
 # conteúdo da estrutura na tela.
 
-dic = {}
-dic['Nome'] = input('Digite o nome do aluno: ')
-dic['Média'] = float(input(f'Digite a média de {dic["Nome"]}: '))
-if dic['Média'] > 7:
+dic = {'Nome': input('Digite o nome do aluno: '), 'Média': float(input('Digite a média: '))}
+
+if dic['Média'] >= 7:
     dic['Situação'] = 'Aprovado'
 else:
     dic['Situação'] = 'Reprovado'
