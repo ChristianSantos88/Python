d1ab7d7e2aa76bc53f9487abc04f4225f33e02ad
Christian dos Santos Silva <19chsantos88@gmail.com>
2022-02-08 11:05:42 -0300

    ex092

--- Exercícios de Dicionários/ex092.py
+++ Exercícios de Dicionários/ex092.py
@@ -1,3 +1,19 @@
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário.
 # Se por acaso a CTPS for diferente de zero, o dicionário também receberá o ano de contratação e o salário. Calcule e
 # acrescente, além da idade, com quantos anos a pessoa vai se aposentar. (Considerese 35 anos de contribuição)
+
+import datetime
+
+dicio = {'Nome': input('Digite o nome: '), 'Ano': int(input('Ano de nascimento: ')), 'CTPS': int(input('Nº da CTPS: '))}
+dicio['Idade'] = datetime.date.today().year - dicio['Ano']
+
+if dicio['CTPS'] != 0:
+    dicio['Admissão'] = int(input('Ano de admissão no trabalho: '))
+    dicio['Salary'] = float(input('Salário: R$ '))
+    aposentadoria = (dicio['Admissão'] - dicio['Ano'] + 35)
+    print(dicio)
+    print(f'{dicio["Nome"]} irá se aposentar com {aposentadoria} anos.')
+else:
+    print(dicio)
+
+
